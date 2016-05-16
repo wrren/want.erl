1bfa0630d277b494539b93b7704546a18d10414f
Warren Kenny <wkenny@riotgames.com>
2016-05-16 11:59:49 +0100

    Expanded tests

--- test/want_tests.erl
+++ test/want_tests.erl
@@ -3,7 +3,10 @@
 
 -include_lib( "eunit/include/eunit.hrl" ).
 
-% This should fail
-basic_test() ->
-    ?assert( want:string( 1 ) 		=:= "1" ),
-	?assert( want:integer( "1" )	=:= 1 ).
+want_test() ->
+    ?assert( want:string( 1 ) 			=:= "1" ),
+	?assert( want:integer( "1" )		=:= 1 ),
+	?assert( want:binary( 1 ) 			=:= <<"1">> ),
+	?assert( want:boolean( true )		=:=	true ),
+	?assert( want:float( <<"1.0">> )	=:= 1.0 ),
+	?assert( want:integer( <<"1">> )	=:= 1 ).
