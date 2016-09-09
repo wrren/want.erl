2747b94a9e2f1ef4d7d4ca433e4801382566b28c
Warren Kenny <wkenny@riotgames.com>
2016-09-09 17:13:01 +0100

    Fixed atom to binary conversion

--- src/want.erl
+++ src/want.erl
@@ -7,6 +7,7 @@
 %%	Convert the given value to a binary
 %%
 binary( V ) when is_binary( V ) 	    -> V;
+binary( V ) when is_atom( V )           -> atom_to_binary( V, utf8 );
 binary( V ) when is_float( V )		    -> float_to_binary( V );
 binary( V ) when is_integer( V )	    -> integer_to_binary( V );
 binary( V ) when is_list( V )		    -> list_to_binary( V );
--- test/want_tests.erl
+++ test/want_tests.erl
@@ -7,6 +7,7 @@ want_test() ->
     ?assert( want:string( 1 ) 			=:= "1" ),
 	?assert( want:integer( "1" )		=:= 1 ),
 	?assert( want:binary( 1 ) 			=:= <<"1">> ),
+	?assert( want:binary( '1' )			=:= <<"1">> ),
 	?assert( want:boolean( true )		=:=	true ),
 	?assert( want:float( <<"1.0">> )	=:= 1.0 ),
 	?assert( want:string( hello )		=:= "hello" ),
