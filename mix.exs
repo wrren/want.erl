7c16bcccd9c0ca1fb0ef3da81b5ee80dbf70562b
wrren <me@wrren.me>
2024-11-16 20:31:32 +0000

    Bumped version number

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Want.MixProject do
   def project do
     [
       app:          :want,
-      version:      "1.10.1",
+      version:      "1.11.0",
       elixir:       "~> 1.10",
       deps:         deps(),
       description:  description(),
